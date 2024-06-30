914f6e8f52a395ffb0a8bfafd97b7d1c495466d0
Kirillefr <161359875+Kirillefr@users.noreply.github.com>
2024-06-30 18:31:31 +0300

    Add files via upload

---
+++ LR.Film3.js
@@ -0,0 +1,33 @@
+document.getElementById("open-modal-btn").addEventListener("click", function(){
+	document.getElementById("my-modal").classList.add("open")
+})
+
+document.getElementById("close-my-modal-btn").addEventListener("click", function(){
+		document.getElementById("my-modal").classList.remove("open")
+})
+
+
+	document.getElementById("open-modal-btn2").addEventListener("click", function(){
+		document.getElementById("my-modal2").classList.add("open2")
+	})
+
+	document.getElementById("close-my-modal-btn2").addEventListener("click", function(){
+			document.getElementById("my-modal2").classList.remove("open2")
+	})
+
+
+		document.getElementById("open-modal-btn3").addEventListener("click", function(){
+			document.getElementById("my-modal3").classList.add("open3")
+		})
+
+		document.getElementById("close-my-modal-btn3").addEventListener("click", function(){
+				document.getElementById("my-modal3").classList.remove("open3")
+		})
+		
+			document.getElementById("open-modal-btn4").addEventListener("click", function(){
+			document.getElementById("my-modal4").classList.add("open4")
+			})
+
+			document.getElementById("close-my-modal-btn4").addEventListener("click", function(){
+					document.getElementById("my-modal4").classList.remove("open4")
+			})
